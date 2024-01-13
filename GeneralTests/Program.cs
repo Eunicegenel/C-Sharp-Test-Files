04d43d21d1f1ff963ad910def8793a84d7b29ee9
Eunel Soliveres <eunel_soliveres@yahoo.com>
2024-01-13 11:48:15 +0800

    Updated Test Project

--- GeneralTests/Program.cs
+++ GeneralTests/Program.cs
@@ -1,5 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Enter your Name:");
-string test = Console.ReadLine();
-Console.Beep();
-Console.WriteLine(test + "... Your name sucks!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your Name:");
+        string test = Console.ReadLine();
+        Console.Beep();
+        Console.WriteLine(test + "... Your name sucks!");
+    }
+}
